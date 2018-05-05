d9b2d9c1dc6ed617061998e12e0c140f53c139da
Decimoseptimo <decimoseptimo@gmail.com>
2018-05-05 09:41:43 -0700

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,46 @@
 module.exports = {
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-plugin-favicon`,
-            options: {
-                logo: `./src/favicon.png`,
-                injectHTML: true,
-                icons: {
-                    android: true,
-                    appleIcon: true,
-                    appleStartup: true,
-                    coast: false,
-                    favicons: true,
-                    firefox: true,
-                    twitter: false,
-                    yandex: false,
-                    windows: false
-                }
-            }
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `img`,
-                path: `${__dirname}/src/img/`
-            }
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-    ],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: `./src/favicon.png`,
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/img/`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-112633310-3",
+        // Puts tracking script in the head instead of the body
+        // head: false,
+        // Setting this parameter is optional
+        // anonymize: true,
+        // Setting this parameter is also optional
+        // respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
 };
