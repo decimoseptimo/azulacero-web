ba6bc8b71f9fb8358d80eb91ff4ca625acdfb57d
Decimoseptimo <decimoseptimo@gmail.com>
2018-07-21 19:50:38 -0700

    Add clicky analytics

---
+++ src/html.js
@@ -0,0 +1,64 @@
+import React from "react"
+
+let stylesStr
+if (process.env.NODE_ENV === `production`) {
+  try {
+    stylesStr = require(`!raw-loader!../public/styles.css`)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports = class HTML extends React.Component {
+  render() {
+    let css
+    if (process.env.NODE_ENV === `production`) {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: stylesStr }}
+        />
+      )
+    }
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          {this.props.headComponents}
+          {css}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+		  <script
+  dangerouslySetInnerHTML={{
+    __html: `
+            var clicky_site_ids = clicky_site_ids || [];
+clicky_site_ids.push(66596048);
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
+        `,
+  }}
+/>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66596048ns.gif" /></p></noscript>
+
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    )
+  }
+}
